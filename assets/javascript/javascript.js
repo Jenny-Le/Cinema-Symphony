378452e55b16f2d79e5abdcee5557b3435ab6b13
Jenny-Le <lejenny75@gmail.com>
2017-12-09 15:22:53 -0700

    movieposterfunction

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+ $("button").on("click", function() {
+    var title = $("#searchText").val();
+    var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=d840ff4";
+    var posterImage = "http://img.omdbapi.com/?apikey=d840ff4";
+    var posterTag = $("<img id='posterImg'>");
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+        console.log(response.Plot)
+        console.log(response.Runtime);
+        console.log(response.Poster)
+
+       $("#moviePoster").append(posterTag); 
+       $('#posterImg').attr("src", response.Poster); 
+
+ 	console.log(title);
+    });
+
+
+
+
+ });
+
+
+
+});
