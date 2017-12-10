82f67013d4d34d408744c3592df39eefb1777c6e
Jenny-Le <lejenny75@gmail.com>
2017-12-10 12:39:43 -0700

    spotifypush

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,32 +1,83 @@
 $(document).ready(function() {
- $("button").on("click", function() {
-    var title = $("#searchText").val();
-    var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=d840ff4";
-    var posterImage = "http://img.omdbapi.com/?apikey=d840ff4";
-    var posterTag = $("<img id='posterImg'>");
+    var config = {
+        apiKey: "AIzaSyB-qWgDrndmGHKYBoad0QVjLOwqyNzNhAI",
+        authDomain: "cinema-symphony.firebaseapp.com",
+        databaseURL: "https://cinema-symphony.firebaseio.com",
+        projectId: "cinema-symphony",
+        storageBucket: "cinema-symphony.appspot.com",
+        messagingSenderId: "119826550462"
+    };
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
-        console.log(response);
-        console.log(response.Plot)
-        console.log(response.Runtime);
-        console.log(response.Poster)
+    firebase.initializeApp(config);
+    var database = firebase.database();
 
-       $("#moviePoster").append(posterTag); 
-       $('#posterImg').attr("src", response.Poster);
-       $("#movieSummary").text(response.Plot) 
+    var hash = window.location.hash.substr(1);
 
-     
 
- 	console.log(title);
+    if (hash == "") {
+        window.location = "https://accounts.spotify.com/authorize?redirect_uri=file:///Users/jenny/Desktop/project-1/index.html&client_id=33df74e9fe14409b96de744f35f548b2&response_type=token"
+    }
+
+    var hashParams = hash.split('&').reduce(function(result, item) {
+        var parts = item.split('=');
+        result[parts[0]] = parts[1];
+        return result;
+    }, {});
+
+    console.log(hashParams);
+
+    $("button").on("click", function() {
+        var title = $("#searchText").val();
+        var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=d840ff4";
+        var posterImage = "http://img.omdbapi.com/?apikey=d840ff4";
+        var posterTag = $("<img id='posterImg'>");
+
+
+        var userText = $("#searchText").val().trim()
+        database.ref().push({
+            searchText: userText
+
+        });
+
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            console.log(response);
+            console.log(response.Plot)
+            console.log(response.Runtime);
+            console.log(response.Poster)
+
+
+
+            $("#moviePoster").append(posterTag);
+            $('#posterImg').attr("src", response.Poster);
+            $("#movieSummary").text(response.Plot)
+            let albumName = response.Title
+            let queryURL = "https://api.spotify.com/v1/search?type=album&market=US&limit=1&q=" + albumName
+            $.ajax({
+                type: "GET",
+                url: queryURL,
+                headers: {
+                    "Authorization": "Bearer " + hashParams.access_token
+                },
+                dataType: 'json'
+            }).done(function(response) {
+                console.log(response)
+                let albumID = response.albums.items[0].id;
+                let iframeURL = "https://open.spotify.com/embed?uri=spotify:album:" + albumID;
+                $('#spotify-player').attr('src', iframeURL);
+                $('#spotify-player').show();
+            })
+
+        });
+
+
+
+
     });
-
-
-
-
- });
 
 
 
